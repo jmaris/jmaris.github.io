a89c6751ac43577607fa414f2a25ae217c84766b
Jordan Maris <jman6495@d1stkfactory>
2016-04-26 12:39:50 +0000

    firdt

---
+++ learnvocab.js
@@ -0,0 +1,78 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function changeDirection() {
+	if(direction=="defr") {
+		direction="frde";
+		document.getElementById('direction').innerHTML="Direction : frde";
+		document.getElementById('comment').innerHTML="";
+	}
+	else if(direction=="frde") {
+		direction="defr";
+		document.getElementById('direction').innerHTML="Direction : defr";
+		document.getElementById('comment').innerHTML="";
+	}
+	newWord();
+	
+}
+
+function newWord() {
+	document.getElementById('translatedword').value="";
+	if(direction=="frde") {
+		document.getElementById('wordtotranslate').innerHTML=(workingVocab[0][0]);
+	}
+	else if(direction=="defr") {
+		document.getElementById('wordtotranslate').innerHTML=(workingVocab[0][1]);
+	}
+}
+
+function checkVocab() {
+	answer=document.getElementById('translatedword').value;
+	if(direction=="frde"){
+		if(answer==workingVocab[0][1]) {
+			document.body.style.backgroundColor = "lightgreen";
+			document.getElementById('wordtotranslate').innerHTML="Correct !";
+			workingVocab.splice(0,1);
+			document.getElementById('comment').innerHTML="";
+			newWord();
+		}
+		else {
+			document.body.style.backgroundColor = "red";
+			document.getElementById('comment').innerHTML="Incorrect ! Please type the correction : <h1>" + workingVocab[0][1] + "</h1>";
+		}
+	}
+	
+	else if(direction=="defr") {
+		if(answer==workingVocab[0][0]) {
+			document.body.style.backgroundColor = "green";
+			document.getElementById('wordtotranslate').innerHTML="Correct !";
+			workingVocab.splice(0, 1);
+			document.getElementById('comment').innerHTML="";
+			newWord();
+		}
+		else {
+			document.body.style.backgroundColor = "red";
+			document.getElementById('comment').innerHTML="Incorrect ! Please type the correction : <h1>" + workingVocab[0][0] + "</h1>";
+		}
+	}
+	document.getElementById('wordsleft').innerHTML=workingVocab.length + " mots restants !";
+}
+
+
+
