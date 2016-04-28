ec76b1293288addea64224dfb66526f15a877f76
Jordan Maris <jmaris@kydara.com>
2016-04-28 11:04:46 +0200

    vocabidontknow

---
+++ vocablists/thevocabicantlearn.js
@@ -0,0 +1,24 @@
+function loadvocab() {
+vocab = [
+	["le projet de loi","der Gesetzentwurf"],
+	["exception","ausnahme"],
+	["approbation","genehmigung"],
+	["l'accord","die Vereinbarung"],
+	["entreprise","Betrieb"],
+	["le concept","der entwurf"],
+	["la primauté","der Vorrang"],
+	["fixer","festsetzen"],
+	["le travail de nuit","der Nachtdienst"],
+	["le décompte","die Abrechnung"],
+	["la dérogation","die Ausnahmegenehmigung"],
+	["autorité administrative","Behörde"],
+	["la compétitivité","die Wettbewerbsfähigkeit"],
+	["réduire","herabstufen"],
+	["un référendum interne","eine betriebliche Abstimmung"],
+	["le droit à la déconnection","das Recht zum Abschalten"],
+	["arbitraire","willkürlich"],
+	["valeur indicative","der Richtwert"],
+	["etwas zulassen","permettre"],
+	["l'assouplissement","die Lockerung"],
+	];
+}
